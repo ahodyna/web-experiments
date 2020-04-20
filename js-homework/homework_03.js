58f15b1038ae53687515dde5a924050830e35aef
alina <hodyna.alina@gmail.com>
2020-04-20 08:47:22 +0300

    month sum

---
+++ js-homework/homework_03.js
@@ -0,0 +1,7 @@
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let sum = 0;
+
+for (let i = 0; i < month.length; i = i + 1){
+    sum = sum + month[i].length
+}
+    console.log (sum)
