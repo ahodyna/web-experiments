c77e7fec928c007f4f22fc1bb4a8c79fa357e7ca
alina <hodyna.alina@gmail.com>
2020-09-20 15:52:13 +0300

    HW-23 extracted cars data to server-side json file

--- js-homework/homework_23_cars_market.js
+++ js-homework/homework_23_cars_market.js
@@ -13,45 +13,20 @@ let carClasses = [
     }
 ]
 
-let cars = [
-    {
-        "name": "Mitsubishi lancer",
-        "price": 12000,
-        "amount": 2
-    },
-    {
-        "name": "BMW X5",
-        "price": 31500,
-        "amount": 3
-    },
-    {
-        "name": "Lada calina",
-        "price": 7900,
-        "amount": 7
-    },
-    {
-        "name": "Audi A8",
-        "price": 28300,
-        "amount": 3
-    },
-    {
-        "name": "Lexus M3",
-        "price": 16500,
-        "amount": 3
-    }
-]
-
-
-
 $(document).ready(function () {
 
     $("#calculateButton").on('click', calculateTotalCost)
     $('#addSectionButton').on('click', addSection) 
-
-    populateTable();
-  
+    console.log("ready body")
 });
 
+
+$.get('./homework_23_cars.json', function(data) {
+    cars = data;
+    populateTable();
+});
+
+let cars = []
 
 
 function populateTable() {
@@ -137,18 +112,3 @@ function deleteCar(i){
 
     populateTable()
 }
-// при загрузке страницы должна отобразится таблица c 5 колонками
-// колонки: class, car, price, amount, total cost
-    // class, - значение должно расчитываться основываясь на цене автомобиля
-    // car, - марка автомобиля
-    // price, - стоимость единицы авто данной марки
-    // amount, - количество автомобилей данной марки на складе
-    // total cost - суммарная стоимость автомобилей даннной марки
-
-// под таблицей следует отобразить суммарную стоимость всех автомобилей
-// суммарная стоимость всех автомобилей расчитывается при на жатии на кнопку calculate total cost, которая так же находится под таблицей
-
-// все элементы пользовательского интерфейса должны быть отцентрированы по центру экрана
-
-// для манипулирования с DOM элементами следует использовать jQuery
-// для задачи стилей центрирования следует использовать flexbox
