ef57d1be712b2122a22df800290f1ec9e141b602
alina <hodyna.alina@gmail.com>
2020-09-27 12:02:42 +0300

    HW-24 implemented user page

--- js-homework/homework_24_posts.js
+++ js-homework/homework_24_posts.js
@@ -48,6 +48,7 @@ function reCreatePostPage() {
             let $postComponent = createPostComponent(userId, postTitle, postBody, user.name, user.email)
             $postList.append($postComponent)
         }
+        $('#root').html(null)
         $('#root').append($postList)
 
     })
@@ -55,7 +56,60 @@ function reCreatePostPage() {
 
 
 function reCreateUserPage(userId) {
-    // $('#root').html(null)
+    $('#root').html(null)
+
+    $.get('https://jsonplaceholder.typicode.com/users', function (data) {
+        let users = data
+
+        let user = findUserById(users, userId)
+
+        let name = user.name
+        let mail = user.email
+        let street = user.address.street
+
+        let city = user.address.city
+        let nameCompany = user.company.name
+        let bs = user.company.bs
+
+        let componentUserBlock = createUserComponent(userId, name, mail, street, city, nameCompany, bs)
+
+        $('#root').append(componentUserBlock)
+    })
+}
+
+function createUserComponent(userId, name, mail, street, city, companyName, bs) {
+    $componentUser = $('<div class="user"></div>')
+
+    $componentUser.append('<div class="userNameAndMailBlock">' + name + mail + '</div>')
+    $componentUser.append('<div class="addresBlock">' + street + city + '</div>')
+    $componentUser.append('<div class="companyBlock">' + companyName + bs + '</div>')
+
+    let $buttonPosts = $('<button>show user posts</button>')
+    $componentUser.append($buttonPosts)
+
+    let $componentBlock = $('<div id="userPosts"></div>')
+    $componentUser.append($componentBlock)
+
+    $buttonPosts.on('click', function () {
+        $.get('https://jsonplaceholder.typicode.com/posts', function (data) {
+            let posts = data
+            for (let i = 0; i < posts.length; i++) {
+                let postsBody = posts[i].body
+                let postTitle = posts[i].title
+
+                let componentPostsTitleUser = $('<div class="postTitle"> ' + postTitle + '</div>')
+                let componentPostsBodyUser = $('<div class="postBody"> ' + postsBody + '</div>')
+
+                let $componentUserPosts = $('<div class="post"></div>')
+
+                $componentUserPosts.append(componentPostsTitleUser)
+                $componentUserPosts.append(componentPostsBodyUser)
+
+                $componentBlock.append($componentUserPosts)
+            }
 
 
+        })
+    })
+    return $componentUser
 }
