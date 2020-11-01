87a8098060de887513dc363e674943c0d284f987
alina <hodyna.alina@gmail.com>
2020-11-01 09:14:43 +0200

    IDX-35 nested loops

---
+++ js-experiments/index_35_nested_loops.js
@@ -0,0 +1,22 @@
+// node.exe .\js-experiments\index_35_nested_loops.js
+
+function print(str) {
+    process.stdout.write(str)    
+}
+
+
+
+print("a")
+print('l')
+process.stdout.write('i')
+process.stdout.write('n')
+process.stdout.write('a')
+process.stdout.write('\n')
+
+process.stdout.write('a')
+process.stdout.write('l')
+process.stdout.write('i')
+process.stdout.write('n')
+process.stdout.write('a')
+process.stdout.write('\n')
+
