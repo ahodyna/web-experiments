de53471dfbe3d8058c6e06961183fd4a27c44489
alina <hodyna.alina@gmail.com>
2020-11-01 09:20:44 +0200

    HW-25 promise

---
+++ js-homework/homework_25_promise.js
@@ -0,0 +1,42 @@
+console.log('Request data...')
+
+// setTimeout(()=> {
+//     console.log('Preparing data...')
+
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+//      setTimeout(() => {
+//          backendData.modified = true
+//         console.log ('Data received', backendData)
+//      },2000 )
+// }, 2000 )
+
+const p = new Promise (function(resolve, reject){
+    setTimeout(() => {
+        console.log('Prepering Data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000)
+})
+
+p.then(data => {
+return new Promise (function (resolve, reject){
+setTimeout(() => {
+    data.modified = true
+    resolve (data)
+}, 2000)
+})
+
+}).then(clientData => {
+    console.log ('Data received', clientData)
+})
+
+.catch(err => console.error('Error: ', err))
+.finally(()=> console.log('Finally'))
