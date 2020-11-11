67498c5b9402199a39d947ff4ba00c2bb8edda45
alina <hodyna.alina@gmail.com>
2020-11-12 00:39:28 +0200

    HW-28 asci art rhombus

--- js-homework/homework_28_console_art_3.js
+++ js-homework/homework_28_console_art_3.js
@@ -2,18 +2,25 @@ function print(str) {
     process.stdout.write(str)
 }
 
-function drawPlus(i, j){
+function drawPlus(i, j, squareSize) {
+    return i == (squareSize - 1) / 2 - j
+        || i == (squareSize - 1) / 2 + j
+        || i == j - (squareSize - 1) / 2
+        || i == (squareSize - 1) / 2 - j + (squareSize - 1)
 
 }
 
-function drawRhombus(squareSize){
-    for (let i = 0; i < squareSize; i++){
+function drawRhombus(squareSize) {
+    for (let i = 0; i < squareSize; i++) {
         for (let j = 0; j < squareSize; j++) {
-            if (drawPlus(i, j)){
-                print ('+')
-            }else {
-                print ('0')
+            if (drawPlus(i, j, squareSize)) {
+                print('+')
+            } else {
+                print('0')
             }
         }
+        print('\n')
     }
 }
+
+drawRhombus(7)
