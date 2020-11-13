4c7572487273df336bd478736d4019e00fa3a593
alina <hodyna.alina@gmail.com>
2020-11-13 22:24:07 +0200

    HW-28 improved drawing function to work with odd & even numbers

--- js-homework/homework_28_console_art_3.js
+++ js-homework/homework_28_console_art_3.js
@@ -2,18 +2,34 @@ function print(str) {
     process.stdout.write(str)
 }
 
-function drawPlus(i, j, squareSize) {
+function drawPlusForEvenNumber(i, j, squareSize) {
+    return i == (squareSize - 2) / 2 - j
+        || i == (squareSize - 2) / 2 + j + 1
+        || i == (j - 1) - (squareSize - 2) / 2
+        || i == ((squareSize - 2) / 2 + 1) - j + 1 + (squareSize - 2)
+}
+
+function drawPlusForOddNumber(i, j, squareSize) {
     return i == (squareSize - 1) / 2 - j
         || i == (squareSize - 1) / 2 + j
         || i == j - (squareSize - 1) / 2
         || i == (squareSize - 1) / 2 - j + (squareSize - 1)
-
 }
+
+function shouldDrawPlus(i, j, squareSize) {
+    if (squareSize % 2 == 0) {
+        return drawPlusForEvenNumber(i, j, squareSize)
+    }
+    else {
+        return drawPlusForOddNumber(i, j, squareSize)
+    }
+}
+
 
 function drawRhombus(squareSize) {
     for (let i = 0; i < squareSize; i++) {
         for (let j = 0; j < squareSize; j++) {
-            if (drawPlus(i, j, squareSize)) {
+            if (shouldDrawPlus(i, j, squareSize)) {
                 print('+')
             } else {
                 print('0')
