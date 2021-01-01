cf9ae68ecc4d02884d4a47e3703759f5b05a59e4
alina <hodyna.alina@gmail.com>
2021-01-01 16:25:15 +0200

    added vertical movement for boarding game

--- js-homework/homework_29.js
+++ js-homework/homework_29.js
@@ -1,16 +1,31 @@
 const square = document.getElementById('square');
 let hDirection = 'right';
+let vDirection = 'down';
 
 setInterval(() => {
     if (!square.style.left) {
         square.style.left = "0px";
+        square.style.top = "0px";
     } else {
+        let positionTopNumber = getPixelNumber(square.style.top);
         let positionLeftNumber = getPixelNumber(square.style.left);
 
         if (positionLeftNumber >= 550) {
             hDirection = "left";
         } else if (positionLeftNumber == 0) {
             hDirection = "right";
+        }
+
+        if (positionTopNumber >= 450){
+            vDirection = "up";
+        } else if(positionTopNumber == 0){
+            vDirection = "down";
+        }
+
+        if (vDirection == "up"){
+            square.style.top = positionTopNumber - 50 + 'px';
+        } else if(vDirection == "down"){
+            square.style.top = positionTopNumber + 50 + 'px';
         }
 
         if (hDirection == "right") {
