4b78c9035b1d935dd9709f6c9fd0bedba1208481
alina <hodyna.alina@gmail.com>
2021-01-23 13:04:53 +0200

    HW-31 imlemented word counter

---
+++ js-homework/homework_31_map.js
@@ -0,0 +1,107 @@
+let tours = [
+    {
+        "id": "507f191e000c1a56ade10000",
+        "name": "Bovaria lakes",
+        "cost": 230,
+        "description": "Beautiful lakes at yor service. \nThe Alpsee is a lake in the Ostallgäu district of Bavaria, Germany, located about 4 kilometres southeast of Füssen. It is close to the Neuschwanstein and Hohenschwangau castles. \nThe Langwieder See lies to the west of Munich by the A8 Autobahn to Stuttgart and was created by the excavation of gravel for motorway construction in the 1930s. It covers an area of approximately 18 hectares (44 acres), is 8 metres deep at its deepest point, and has a shoreline of 2.2 kilometres (1.4 mi). It is the second largest standing body of water in Munich. \nBavaria shares international borders with Austria (Salzburg, Tyrol, Upper Austria and Vorarlberg) and Czechia (Karlovy Vary, Plzeň and South Bohemian Regions), as well as with Switzerland (across Lake Constance to the Canton of St. Gallen). Because all of these countries are part of the Schengen Area, the border is completely open. Neighboring states within Germany are Baden-Württemberg, Hesse, Thuringia, and Saxony. Two major rivers flow through the state: the Danube (Donau) and the Main. The Bavarian Alps define the border with Austria (including the Austrian federal-states of Vorarlberg, Tyrol and Salzburg), and within the range is the highest peak in Germany: the Zugspitze. The Bavarian Forest and the Bohemian Forest form the vast majority of the frontier with the Czechia and Bohemia.",
+        "details": {
+            "transportType": "Bus",
+            "durationDays": 3
+        }
+    },
+    {
+        "id": "507f191e000c1a56ade10001",
+        "name": "Wild France",
+        "cost": 1210,
+        "description": "Wild nature of Frace at yor service. \nAiguilles du Dru is located within the Mont Blanc mountain range. Though its largest peak is only 3,755 meters high, far less than that of Mont Blanc itself, Aiguilles du Dru is still quite striking in its own rite. Aiguilles du Dru is made of granite rock. It is known for its grey rock fall scar and its jagged peak, and is aptly named because ‘aiguille’ means ‘needle’. There is a lower summit of 3,733 meters that is also considered part of the Aiguilles du Dru, and it is named The Petit Dru. People have been climbing this peak at least since 1878. In 1913 climbers placed a metal statue of Our Lady of Lourdes at the summit, and it still stands today. \nMetropolitan France has a wide variety of topographical sets and natural landscapes. Large parts of the current territory of France were raised during several tectonic episodes like the Hercynian uplift in the Paleozoic Era, during which the Armorican Massif, the Massif Central, the Morvan, the Vosges and Ardennes ranges and the island of Corsica were formed. These massifs delineate several sedimentary basins such as the Aquitaine basin in the southwest and the Paris basin in the north, the latter including several areas of particularly fertile ground such as the silt beds of Beauce and Brie. Various routes of natural passage, such as the Rhône valley, allow easy communications. The Alpine, Pyrenean and Jura mountains are much younger and have less eroded forms. At 4,810.45 metres (15,782 ft)[97] above sea level, Mont Blanc, located in the Alps on the French and Italian border, is the highest point in Western Europe. Although 60% of municipalities are classified as having seismic risks, these risks remain moderate.",
+        "details": {
+            "transportType": "Plane",
+            "durationDays": 5
+        }
+    },
+    {
+        "id": "507f191e000c1a56ade10002",
+        "name": "Côte d'Azur croatia",
+        "cost": 324,
+        "description": "Croatia is located in Central and Southeast Europe, on the coast of the Adriatic Sea. It borders Hungary to the northeast, Serbia to the east, Bosnia and Herzegovina and Montenegro to the southeast, and Slovenia to the northwest. It lies mostly between latitudes 42° and 47° N and longitudes 13° and 20° E. Part of the territory in the extreme south surrounding Dubrovnik is a practical exclave connected to the rest of the mainland by territorial waters, but separated on land by a short coastline strip belonging to Bosnia and Herzegovina around Neum. \nThe territory covers 56,594 square kilometres (21,851 square miles), consisting of 56,414 square kilometres (21,782 square miles) of land and 128 square kilometres (49 square miles) of water. It is the 127th largest country in the world.[117] Elevation ranges from the mountains of the Dinaric Alps with the highest point of the Dinara peak at 1,831 metres (6,007 feet) near the border with Bosnia and Herzegovina in the south[117] to the shore of the Adriatic Sea which makes up its entire southwest border. Insular Croatia consists of over a thousand islands and islets varying in size, 48 of which are permanently inhabited. The largest islands are Cres and Krk,[117] each of them having an area of around 405 square kilometres (156 square miles)",
+        "details": {
+            "transportType": "Bus",
+            "durationDays": 4
+        }
+    },
+    {
+        "id": "507f191e000c1a56ade10003",
+        "name": "Asian paradise",
+        "cost": 2050,
+        "description": "Thailand, officially the Kingdom of Thailand and formerly known as Siam, is a country in Southeast Asia. Located at the centre of the Indochinese Peninsula, it is composed of 76 provinces, and covers an area of 513,120 square kilometres (198,120 sq mi), and a population of over 66 million people. Thailand is the world's 50th-largest country by land area, and the 22nd-most-populous country in the world. The capital and largest city is Bangkok, a special administrative area. Thailand is bordered to the north by Myanmar and Laos, to the east by Laos and Cambodia, to the south by the Gulf of Thailand and Malaysia, and to the west by the Andaman Sea and the southern extremity of Myanmar. Its maritime boundaries include Vietnam in the Gulf of Thailand to the southeast, and Indonesia and India on the Andaman Sea to the southwest. Nominally, Thailand is a constitutional monarchy and parliamentary democracy; however, in recent history, its government has experienced multiple coups and periods of military dictatorships. \nTai peoples migrated from southwestern China to mainland Southeast Asia from the 11th century; the oldest known mention of their presence in the region by the exonym Siamese dates to the 12th century. Various Indianised kingdoms such as the Mon kingdoms, Khmer Empire and Malay states ruled the region, competing with Thai states such as the Kingdoms of Ngoenyang, Sukhothai, Lan Na and Ayutthaya, which rivalled each other. Documented European contact began in 1511 with a Portuguese diplomatic mission to Ayutthaya, which became a regional power by the end of the 15th century. Ayutthaya reached its peak during cosmopolitan Narai's reign (1656–1688), gradually declining thereafter until being ultimately destroyed in the 1767 Burmese–Siamese War. Taksin (r. 1767–1782) quickly reunified the fragmented territory and established the short-lived Thonburi Kingdom. He was succeeded in 1782 by Buddha Yodfa Chulaloke (r. 1782–1809), the first monarch of the current Chakri dynasty.",
+        "details": {
+            "transportType": "Plane",
+            "durationDays": 7
+        }
+    },
+    {
+        "id": "507f191e000c1a56ade10004",
+        "name": "Magnificent Turkey",
+        "cost": 2050,
+        "description": "First appearing in the Ottoman Empire, under the strictest interpretations of the Quran the strong coffee was considered a drug and its consumption was forbidden. Due to the immense popularity of the beverage, the sultan eventually lifted this prohibition. \nTurkish coffee culture had reached Britain and France by the mid to late 17th century. The first coffee house in Britain was opened by a Turkish Jew in the mid 17th century. In the 1680s, the Turkish ambassador to France reportedly threw lavish parties for the city's elite where African slaves served coffee to guests in porcelain finjans on gold or silver saucers. \nHagia Sophia (/ˈhɑːɡiə soʊˈfiːə/; from Koinē Greek: Ἁγία Σοφία, romanized: Hagía Sophía; Latin: Sancta Sophia, lit. 'Holy Wisdom'), officially the Hagia Sophia Grand Mosque (Turkish: Ayasofya-i Kebir Cami-i Şerifi) and formerly the Church of Hagia Sophia, is a Late Antique place of worship in Istanbul. Built in 537 as the patriarchal cathedral of the imperial capital of Constantinople, it was the largest Christian church of the eastern Roman Empire (the Byzantine Empire) and the Eastern Orthodox Church, except during the Latin Empire from 1204 to 1261, when it became the city's Roman Catholic cathedral. In 1453, after the Fall of Constantinople to the Ottoman Empire, it was converted into a mosque. In 1935 the secular Turkish Republic established it as a museum. In 2020, it re-opened as a mosque. ",
+        "details": {
+            "transportType": "Plane",
+            "durationDays": 7
+        }
+    }
+]
+
+// Определить самое популярное слово длиннее 3 символов в описании для каждого тура
+// В результате работы программы в консоли долно быть напечатанны названия туров, самое популярное слово для них и количество повторений этих слов
+// Фомат вывода на консоль: "Test tour - with - 2"
+
+// пример работы с одним объектом
+// {
+//     "id": "507f191e000c1a56ade10003",
+//     "name":"Test tour",
+//     "cost": 123,
+//     "description": "A super beautiful tour with a cool nature trips. This tour includes acquaintance with a pure nature of country",
+//     "details": {
+//         "transportType": "Plane",
+//         "durationDays": 7
+//     }
+// }
+
+// Следует проанализировать слова в поле description
+// Cамым популярным словом для этого тура будет "with", т.к. оно длиннее 3 символов и встречается 2 раза.
+
+function findPopularWord(tour) {
+
+        const descriptionWords = tour.description.split(/\W+/);
+        const map = new Map();
+
+        for (let j = 0; j < descriptionWords.length; j++) {
+            
+            const descriptionToken = descriptionWords[j]
+            if (descriptionToken.length > 3) {
+                
+                if (map.has(descriptionToken)) {
+                    map.set(descriptionToken, map.get(descriptionToken) + 1);
+                } else {
+                    map.set(descriptionToken, 1)
+                }
+            }
+        }
+        let counter = 0;
+        let word = '';
+        for (let [key, value] of map) {
+            if (value > counter) {
+                counter = value;
+                word = key;
+            }
+        }
+    return {  
+        counter : counter,
+        word : word,
+    }
+}
+
+for (let i = 0; i < tours.length; i++){
+    let result = findPopularWord(tours[i]);
+    console.log(tours[i].name + ' - ' + result.counter + ' - ' + result.word)
+}
